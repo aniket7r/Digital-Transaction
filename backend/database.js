80a6c220cf4308652af88a181498cabc00268293
aniket7r <aniketkumar6256@gmail.com>
2024-05-20 14:46:08 +0530

    Mongoose Schema Created and connected to Mongodb

---
+++ backend/database.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+mongoose.connect("mongodb+srv://aniketkumar6256:earthware0306@cluster0.lsqwmdr.mongodb.net/DigitalTransaction")
+
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+	User,
+    Account
+};
